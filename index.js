a62c712f761bc3ff6dbee097ab9899d72c452d58
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-21 22:43:01 -0800

    Pulling down coin info from CoinGecko API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ fetch(
 )
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    // console.log(data);
     // change to regular temporarily for loading time
     let imgURL = data.urls.full;
 
@@ -12,3 +12,11 @@ fetch(
       "author-info"
     ).textContent = `By: ${data.user.name}`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    // .market_data.current_price.usd
+  })
+  .catch((err) => console.error(err));
